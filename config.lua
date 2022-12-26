2d68471511562a129b159d11a9b83ea0d3bb49a9
artistic-nucleus <103202629+artistic-nucleus@users.noreply.github.com>
2022-12-26 22:02:11 +0530

    added formatters

--- config.lua
+++ config.lua
@@ -126,19 +126,19 @@ vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "clangd" })
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-    -- { command = "stylua", filetypes = { "lua" } },
-    --   { command = "black", filetypes = { "python" } },
-    --   { command = "isort", filetypes = { "python" } },
-    { command = "prettierd", filetypes = { "html", "css" } },
-    --   {
-    --     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    --     command = "prettier",
-    --     ---@usage arguments to pass to the formatter
-    --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    --     extra_args = { "--print-width", "100" },
-    --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    --     filetypes = { "typescript", "typescriptreact" },
-    --   },
+  -- { command = "stylua", filetypes = { "lua" } },
+  { command = "black", filetypes = { "python" } },
+  --   { command = "isort", filetypes = { "python" } },
+  { command = "prettierd", filetypes = { "html", "css", "typescript", "typescriptreact", "markdown" } },
+  --   {
+  --     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+  --     command = "prettier",
+  --     ---@usage arguments to pass to the formatter
+  --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+  --     extra_args = { "--print-width", "100" },
+  --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+  --     filetypes = { "typescript", "typescriptreact" },
+  --   },
 })
 
 -- -- set additional linters
