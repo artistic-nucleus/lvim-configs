0d6312b4d39505e7d9d30f96afcebf4c721cb814
artistic-nucleus <103202629+artistic-nucleus@users.noreply.github.com>
2022-12-26 22:02:48 +0530

    added qflist shortcuts

--- config.lua
+++ config.lua
@@ -40,21 +40,23 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  --   -- for input mode
+  i = {
+    --     ["<C-j>"] = actions.move_selection_next,
+    --     ["<C-k>"] = actions.move_selection_previous,
+    --     ["<C-n>"] = actions.cycle_history_next,
+    --     ["<C-p>"] = actions.cycle_history_prev,
+    ["<C-a>"] = actions.smart_add_to_qflist + actions.open_qflist,
+  },
+  --   -- for normal mode
+  n = {
+    --     ["<C-j>"] = actions.move_selection_next,
+    --     ["<C-k>"] = actions.move_selection_previous,
+    ["<C-a>"] = actions.smart_add_to_qflist + actions.open_qflist,
+  },
+}
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
